6cf67d138e15f256a1c1783802b629b44791b984
berl weber <153825363+berlweber@users.noreply.github.com>
2026-01-20 15:21:30 +0100

    finished get index route

--- controllers/projects.js
+++ controllers/projects.js
@@ -6,9 +6,13 @@ import Project from '../models/project.js';
 // GET index projects
 router.get('/', async (req, res) => {
     try {
-        
+        const allProjects = await Project.find({});
+        res.render('projects/index.ejs', {
+            projects: allProjects,
+        });
     } catch (error) {
         console.log(error.message);
+        res.redirect('/');
     }
 })
 
