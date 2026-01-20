503d42e037b48e1d9176c887aa240f9757834bbc
berl weber <153825363+berlweber@users.noreply.github.com>
2026-01-20 12:43:25 +0100

    created the Project model

---
+++ models/project.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const projectSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    discription: {
+        type: text,
+        required: false,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+export default Project;
