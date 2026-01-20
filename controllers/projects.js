7502861a9424f3816d0c6554d4bac9a3c3f69f23
berl weber <153825363+berlweber@users.noreply.github.com>
2026-01-20 17:03:26 +0100

    created the new project post route

--- controllers/projects.js
+++ controllers/projects.js
@@ -21,4 +21,15 @@ router.get('/new', (req, res) => {
     res.render('projects/new.ejs');
 });
 
+router.post('/', async (req, res) => {
+    try {
+        req.body.owner = req.session.user._id;
+        await Project.create(req.body);
+        res.redirect('/projects');
+    } catch (error) {
+        console.log(error.message);
+        res.redirect('/projects');
+    }
+});
+
 export default router;
