5606fb4b78c9cc615369409dc424a48438fac089
berl weber <153825363+berlweber@users.noreply.github.com>
2026-01-21 20:45:43 +0100

    finished POST route to functional level

--- controllers/documents.js
+++ controllers/documents.js
@@ -8,20 +8,30 @@ import Project from '../models/project.js';
 // });
 
 // create GET route
-router.get('/new', async (req, res) => {
+router.get('/:projectId/new', async (req, res) => {
     const allProjects = await Project.find({ owner: req.session.user._id });
     res.render('documents/new.ejs', {
         projects: allProjects,
+        thisProject: req.params.projectId,
     });
 });
 
 // POST new doc route
-router.post('/', async (req, res) => {
+router.post('/:projectId', async (req, res) => {
     try {
-        req.body.owner = req.session.user._id;
-        
-        await Document.create(req.body);
-        res.redirect('/documents');
+        const project = await Project.findById(req.params.projectId).populate('owner');
+        if (project.owner.equals(req.session.user._id) ) {
+            req.body.owner = req.session.user._id;
+            req.body.project = project._id;
+            // if (req.body.parentDoc) >>> asign parentDoc id
+            if (req.body.done) req.body.done = true;
+            console.log(req.body);
+            await Document.create(req.body);
+            res.redirect('/projects');//change this one with the following after availibe
+            // res.redirect(`projects/:${project._id}`);
+        } else {
+        res.send('You do not have the permission to add documents to this project');
+        }
     } catch (error) {
         console.log(error.message);
         res.redirect('/documents');
