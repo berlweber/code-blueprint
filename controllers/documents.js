d081fb121218fd773b7f98e0aef7ff0f441580ee
berl weber <153825363+berlweber@users.noreply.github.com>
2026-01-21 15:31:58 +0100

    created the create route

--- controllers/documents.js
+++ controllers/documents.js
@@ -1,9 +1,18 @@
 import express from 'express';
 const router = express.Router();
 import Document from '../models/document.js';
+import Project from '../models/project.js';
 
-router.get('/', (req, res) => {
-    res.send('works!!!!!!!!!!!!!!!!!');
+// router.get('/', (req, res) => {
+//     res.send('works!!!!!!!!!!!!!!!!!');
+// });
+
+// create route
+router.get('/new', async (req, res) => {
+    const allProjects = await Project.find({ owner: req.session.user._id });
+    res.render('documents/new.ejs', {
+        projects: allProjects,
+    });
 });
 
 export default router;
