8d49a51969699fdf4adca5854aa35885b934d76d
berl weber <153825363+berlweber@users.noreply.github.com>
2026-01-21 15:47:55 +0100

    created basic post doc route

--- controllers/documents.js
+++ controllers/documents.js
@@ -7,12 +7,25 @@ import Project from '../models/project.js';
 //     res.send('works!!!!!!!!!!!!!!!!!');
 // });
 
-// create route
+// create GET route
 router.get('/new', async (req, res) => {
     const allProjects = await Project.find({ owner: req.session.user._id });
     res.render('documents/new.ejs', {
         projects: allProjects,
     });
+});
+
+// POST new doc route
+router.post('/', async (req, res) => {
+    try {
+        req.body.owner = req.session.user._id;
+        
+        await Document.create(req.body);
+        res.redirect('/documents');
+    } catch (error) {
+        console.log(error.message);
+        res.redirect('/documents');
+    }
 });
 
 export default router;
